cdc4e7031a4b96643144d8da4b656fb00b78b488
dKay <danil.kayorodov@mail.ru>
2025-01-17 19:42:01 +0300

    added main slider

--- components/blocks/Slider.jsx
+++ components/blocks/Slider.jsx
@@ -1,74 +1,107 @@
-import s from '/Users/danil/mok/styles/Home.module.scss'
-import Image from 'next/image'
+import { useState, useEffect } from 'react';
+import s from '/styles/Home.module.scss';
+import Image from 'next/image';
 
-export default function Slider () {
+export default function Slider() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+  const [currentTransition, setCurrentTransition] = useState(0)
+
+  const nextSlide = () => {
+    setCurrentTransition((prevTransition) => {
+        const newSlide = (currentSlide !== slides.length - 1) ? currentSlide + 1 : 0
+        const newTransition = (currentSlide !== slides.length - 1) ? prevTransition - 100 : 0
+
+        setCurrentSlide(newSlide)
+        return newTransition
+    })
+  }
+
+  const goToSlide = (index) => {
+    setCurrentSlide(index)
+    setCurrentTransition(index * -100)
+  }
+
+  useEffect(() => {
+      const interval = setInterval(nextSlide, 5000)
+      return () => clearInterval(interval)
+  }, [currentSlide])
+
+
+  const slides = [
+    { id: 1, image: '/underHeaderBackground.png', header: 'Центр хирургии МОК клиники' },
+    { id: 2, image: '/underHeaderBackground.png', header: 'Стационары для взрослых и детей' },
+    { id: 3, image: '/underHeaderBackground.png', header: 'Собственная служба скорой помощи' },
+    { id: 4, image: '/underHeaderBackground.png', header: 'Консультация хирурга бесплатно' },
+  ];
+
+  return (
+    <section className={s.slider}>
+      <div className={s.slides} style={{ transform: `translateX(${currentTransition}%)`}}>
+        {slides.map((slide, index) => (
+          <Slide key={slide.id} slide={slide} isActive={index === currentSlide} />
+        ))}
+      </div>
+      <div className={s.slideCount}>
+        {slides.map((_, index) => (
+          <div
+            onClick={() => goToSlide(index)}
+            key={index}
+            className={`${s.slideCounter} ${currentSlide === index ? s.slideCounterActive : ''}`}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+function Slide({ slide }) {
     return (
-        <section className={s.slider}>
-            <div className={s.slideInfo}>
-            <h1 className={s.sliderHeader}>Центр хирургии<br />МОК клиники</h1>
-            <div className={s.descriptionContainer}>
-                <div className={s.description}>
-                <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" stroke-width="0.865725"/>
-                </svg>
-                <span className={s.des}>7 хирургических центров: экстренная и плановая хирургия</span>
-                </div>
-                <div className={s.description}>
-                <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" stroke-width="0.865725"/>
-                </svg>
-                <span className={s.des}>Стационары для взрослых и детей</span>
-                </div>
-                <div className={s.description}>
-                <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" stroke-width="0.865725"/>
-                </svg>
-                <span className={s.des}>Собственная служба скорой помощи</span>
-                </div>
-                <div className={s.description}>
-                <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" stroke-width="0.865725"/>
-                    <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" stroke-width="0.865725"/>
-                </svg>
-                <span className={s.des}>Консультация хирурга по поводу операции – бесплатно </span>
-                </div>
-            </div> 
-            <button className={`${s.button0} ${s.buttonMat0} ${s.btn0}`}>Записаться на прием</button>
+    <div className={s.slide}>
+        <div className={s.slideInfo}>
+          <h1 className={s.slideHeader}>{slide.header}</h1>
+          <div className={s.descriptionContainer}>
+            <div className={s.description}>
+              <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" strokeWidth="0.865725" />
+              </svg>
+              <span className={s.des}>7 хирургических центров: экстренная и плановая хирургия</span>
             </div>
-            <picture>
-                <source media="(max-width: 768px)" srcSet="/underHeaderBackgroundPhone.png" />
-                <source media="(min-width: 769px)" srcSet="/underHeaderBackground.png" />
-                <Image
-                className={s.underHeaderBackground}
-                src={'/underHeaderBackground.png'}
-                width={2000}
-                height={2000}
-                />
-            </picture>
-            <Image
-            className={s.XUH}
-            src={'/X.png'}
-            width={100}
-            height={100}
-            />
-            <Image
-            className={s.VUH}
-            src={'/V.png'}
-            width={100}
-            height={100}
-            />
-            <div className={s.slideCount}>
-                <div className={s.slideCounterActive}></div>
-                <div className={s.slideCounter}></div>
-                <div className={s.slideCounter}></div>
+            <div className={s.description}>
+              <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" strokeWidth="0.865725" />
+              </svg>
+              <span className={s.des}>Стационары для взрослых и детей</span>
             </div>
-        </section>
+            <div className={s.description}>
+              <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" strokeWidth="0.865725" />
+              </svg>
+              <span className={s.des}>Собственная служба скорой помощи</span>
+            </div>
+            <div className={s.description}>
+              <svg width="23" height="23" viewBox="0 0 23 23" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <circle cx="11.6692" cy="11.6101" r="10.3887" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M11.6689 6.84814V16.3711" stroke="#391FCF" strokeWidth="0.865725" />
+                <path d="M6.9082 11.6094H16.4312" stroke="#391FCF" strokeWidth="0.865725" />
+              </svg>
+              <span className={s.des}>Консультация хирурга по поводу операции – бесплатно </span>
+            </div>
+          </div>
+          <button className={`${s.button0} ${s.buttonMat0} ${s.btn0}`}>Записаться на прием</button>
+        </div>
+        <picture>
+          <source media="(max-width: 768px)" srcSet="/underHeaderBackgroundPhone.png" />
+          <source media="(min-width: 769px)" srcSet={slide.image} />
+          <Image className={s.underHeaderBackground} src={slide.image} width={2000} height={2000} />
+        </picture>
+        <Image className={s.XUH} src={'/X.png'} width={100} height={100} />
+        <Image className={s.VUH} src={'/V.png'} width={100} height={100} />
+    </div>
     )
 }
