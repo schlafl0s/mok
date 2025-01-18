beeca796088a90f41f8694b3656a42053a64d18c
dKay <danil.kayorodov@mail.ru>
2025-01-18 19:46:39 +0300

    added article page

--- components/blocks/News.jsx
+++ components/blocks/News.jsx
@@ -1,11 +1,11 @@
 import s from '/styles/Home.module.scss'
 import Image from 'next/image'
 
-export default function News () {
+export default function News ({newsName = 'Пресс-центр'}) {
     return (
         <section className={s.news}>
             <div className={s.specialsUp}>
-                <h1 className={s.Header}>Пресс-центр</h1>
+                <h1 className={s.Header}>{newsName}</h1>
                 <div className={s.switcher}>
                     <button className={`${s.switchBtnLeft} ${s.switchInactive}`}>
                         <svg className={s.switchBtnLeftArrow} width="27" height="24" viewBox="0 0 27 24" fill="none" xmlns="http://www.w3.org/2000/svg">
