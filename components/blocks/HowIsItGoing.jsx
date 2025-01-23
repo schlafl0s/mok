61789890e6083644ff29a1b63571c2d6f2798765
dKay <danil.kayorodov@mail.ru>
2025-01-24 00:34:08 +0300

    ended dop prices for services

--- components/blocks/HowIsItGoing.jsx
+++ components/blocks/HowIsItGoing.jsx
@@ -1,16 +1,28 @@
+import { useState } from 'react'
 import s from '/styles/Home.module.scss'
 import Image from 'next/image'
 
 export default function HowIsItGoing () {
+    const data = [
+        { num: '01', header: 'Получите направление (или назначение) на лабораторное исследование', text: 'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.' },
+        { num: '02', header: 'Подготовка к проведению анализа и сдача', text: 'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.' },
+        { num: '03', header: 'Получение результатов лабораторных исследований', text: 'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.' },
+        { num: '04', header: 'Получение результатов лабораторных исследований', text: 'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.' }
+    ];
+
     return (
         <div className={s.how}>
             <h1 className={s.Header}>Как проходит процедура</h1>
             <div className={s.howContainer}>
                 <div className={s.howInfo}>
-                    <Hidden num={'01'} header={'Получите направление (или назначение) на лабораторное исследование'} text={'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.'} />
-                    <Hidden num={'02'} header={'Подготовка к проведению анализа и сдача'} text={'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.'} />
-                    <Hidden num={'03'} header={'Получение результатов лабораторных исследований'} text={'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.'} />
-                    <Hidden num={'04'} header={'Получение результатов лабораторных исследований'} text={'Назначить стандартное лабораторное исследование может врач любой специализации при наличии показаний, так как всем специалистам системы столичного здравоохранения доступен один и тот же перечень возможных лабораторных тестов.'} />
+                    {data.map((item) => (
+                        <Hidden 
+                        key={item.num} 
+                        num={item.num} 
+                        header={item.header} 
+                        text={item.text} 
+                        />
+                    ))}
                 </div>
                 <Image className={s.howImg} src="/how.png" width={2000} height={2000} />
             </div>
@@ -19,22 +31,23 @@ export default function HowIsItGoing () {
 }
 
 function Hidden ({num, header, text}) {
+    const [hidden, setHidden] = useState(false)
+
     return (
-        <button className={s.howHidden}>
-            <div className={s.howLeftHidden}>
-                 <div className={s.howNum}>{num}</div>
-                <h2 className={s.howHeader}>{header}</h2>
+        <button onClick={() => setHidden(!hidden)} className={s.howHidden}>
+            <div className={s.upSide}>
+                <div className={s.howLeftHidden}>
+                    <div className={`${s.howNum} ${hidden ? s.howNumBlue : ''}`}>{num}</div>
+                    <h2 className={s.howHeader}>{header}</h2>
+                </div>
+                <div className={s.howStatus}>
+                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path className={hidden ? s.line : s.unactiveLine} d="M8.7041 0.870117V18.1757" stroke="#5BE146" stroke-width="1.73056"/>
+                        <path className={hidden ? s.line2 : s.unactiveLine2} d="M0.0512695 9.52246L17.3568 9.52246" stroke="#5BE146" stroke-width="1.73056"/>
+                    </svg>
+                </div>
             </div>
-            <div className={s.howStatus}>
-                <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M8.7041 0.870117V18.1757" stroke="#5BE146" stroke-width="1.73056"/>
-                    <path d="M0.0512695 9.52246L17.3568 9.52246" stroke="#5BE146" stroke-width="1.73056"/>
-                </svg>
-                {/* <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.0512695 9.06836L17.3568 9.06836" stroke="#391FCF" stroke-width="1.73056"/>
-                </svg> */}
-            </div>
-            {/* <p className={s.howText}>{text}</p> */}
+            <p className={`${hidden ? s.howText : s.howTextUnactive}`}>{text}</p>
         </button>
     )
 }
--- components/blocks/Prices.jsx
+++ components/blocks/Prices.jsx
@@ -1,39 +1,80 @@
 import s from '/styles/Home.module.scss'
+import { useState } from 'react'
 
 export default function Prices () {
+    const data = [
+        { 
+            header: 'Аллергология', 
+            price: 'от 2300',
+            dopPrices: [
+                { dopHeader: 'Тест на аллергию', dopPrice: '1500' },
+                { dopHeader: 'Анализ крови на аллерген', dopPrice: '800' },
+            ]
+        },
+        { 
+            header: 'Иммуноферментный анализ', 
+            price: 'от 2300',
+            dopPrices: [
+                { dopHeader: 'Тест на иммунитет', dopPrice: '1200' },
+                { dopHeader: 'Тест на аллергию', dopPrice: '1500' },
+                { dopHeader: 'Анализ крови на аллерген', dopPrice: '800' },
+            ]
+        },
+        { 
+            header: 'Гистологические исследования', 
+            price: '3300',
+            dopPrices: [
+                { dopHeader: 'Тест на аллергию', dopPrice: '1500' },
+                { dopHeader: 'Анализ крови на аллерген', dopPrice: '800' },
+                { dopHeader: 'Тест на аллергию', dopPrice: '1500' },
+                { dopHeader: 'Анализ крови на аллерген', dopPrice: '800' },
+            ]
+        }
+    ];
     return (
         <section className={s.prices}>
             <div className={s.pricesBackground}></div>
             <div className={s.pricesInfo}>
                 <h1 className={s.Header}>Цены на диагностику и анализы</h1>
                 <div className={s.pricesContainer}>
-                    <Price header={'Аллергология'} price={'от 2300'}/>
-                    <Price header={'Иммуноферментный анализ'} price={'2300'}/>
-                    <Price header={'Гистологические и иммуногистохимические исследования'} price={'2300'}/>
-                    <Price header={'Аллергология'} price={'2300'}/>
-                    <Price header={'Аллергология'} price={'от 3200'}/>
+                    {data.map((item) => (
+                        <Price 
+                            header={item.header}
+                            price={item.price}
+                            dopPrices={item.dopPrices}
+                        />
+                    ))}
                 </div>
             </div>
         </section>
     )
 }
 
-function Price ({header, price, names, prices}) {
+function Price ({header, price, dopPrices}) {
+    const [hidden, setHidden] = useState(false)
+
     return (
-        <div className={s.price}>
-            <h2 className={s.priceHeader}>{header}</h2>
-            <div className={s.priceRight}>
-                <span className={s.pricePrice}>{price} ₽</span>
-                <div className={s.priceOpen}>
-                    <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M8.7041 0.870117V18.1757" stroke="white" stroke-width="1.73056"/>
-                        <path d="M0.0512695 9.52246L17.3568 9.52246" stroke="white" stroke-width="1.73056"/>
-                    </svg>
-                    {/* <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
-                        <path d="M0.0512695 9.06836L17.3568 9.06836" stroke="#391FCF" stroke-width="1.73056"/>
-                    </svg> */}
+        <button onClick={() => setHidden(!hidden)} className={s.price}>
+            <div className={s.upSidePrice}>
+                <h2 className={`${s.priceHeader} ${hidden ? s.priceHeaderBlue : ''}`}>{header}</h2>
+                <div className={s.priceRight}>
+                    <span className={s.pricePrice}>{price} ₽</span>
+                    <div className={`${s.priceOpen} ${hidden ? s.priceOpenBlue : ''}`}>
+                        <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path className={hidden ? s.linePrice : s.unactiveLinePrice} d="M8.7041 0.870117V18.1757" stroke="#5BE146" stroke-width="1.73056"/>
+                            <path className={hidden ? s.line2Price : s.unactiveLine2Price} d="M0.0512695 9.52246L17.3568 9.52246" stroke="#5BE146" stroke-width="1.73056"/>
+                        </svg>
+                    </div>
                 </div>
             </div>
-        </div>
+            <div className={hidden ? s.dopPrices : s.dopPricesUnactive}>
+                {dopPrices.map((dopItem, index) => (
+                    <div key={index} className={hidden ? s.dopPrice : s.dopPriceUnactive}>
+                        <h3 className={hidden ? s.dopPriceHeader : s.dopPriceHeaderUnactive}>{dopItem.dopHeader}</h3>
+                        <span className={hidden ? s.pricePrice : s.pricePriceUnactive}>{dopItem.dopPrice} ₽</span>
+                    </div>
+                ))}
+            </div>
+        </button>
     )
 }
